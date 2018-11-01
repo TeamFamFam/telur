c7772487cb16246fb6db9491abcbc40b02714ebc
Nathanael See <nathanaelsee@yahoo.com>
2018-10-31 20:38:15 -0700

    Updates

---
+++ precache-manifest.5a2e4d7ad4e6785896019fc13de66db1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b689d0706c27baa77b89",
+    "url": "/static/js/main.b689d070.chunk.js"
+  },
+  {
+    "revision": "c00ef3e3db46ce9dcf47",
+    "url": "/static/js/1.c00ef3e3.chunk.js"
+  },
+  {
+    "revision": "b689d0706c27baa77b89",
+    "url": "/static/css/main.2b93dd0b.chunk.css"
+  },
+  {
+    "revision": "7594a4a3134676806251a6572d106a4e",
+    "url": "/index.html"
+  }
+];
