a21d901a8ae4eac6152bc16343e83132e4f8d113
Nathanael See <nathanaelsee@yahoo.com>
2018-11-06 19:27:19 -0800

    Fix indentation

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -9,18 +9,21 @@ export default class Splash extends Component {
     return (
       <div>
         <h1>Telur</h1>
-          <Input icon='user' iconPosition='left' placeholder='Username'/>
-          <br/><br/>
-          <Input type='password' icon='key' iconPosition='left' placeholder='Password' />
-          <br/><br/><br/>
-              <Link to="/">
-                <Button primary onClick={this.props.toggle}>Sign Up</Button>
-              </Link>
-              <Link to="/">
-                <Button secondary onClick={this.props.toggle}>Log In</Button>
-              </Link>
-              <Redirect to="/" />
-          </div>
+        <Input icon='user' iconPosition='left' placeholder='Username'/>
+        <br/>
+        <br/>
+        <Input type='password' icon='key' iconPosition='left' placeholder='Password' />
+        <br/>
+        <br/>
+        <br/>
+        <Link to="/">
+          <Button primary onClick={this.props.toggle}>Sign Up</Button>
+        </Link>
+        <Link to="/">
+          <Button secondary onClick={this.props.toggle}>Log In</Button>
+        </Link>
+        <Redirect to="/" />
+      </div>
     )
-}
+  }
 }
