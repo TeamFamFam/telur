dc4a1e6c271e550b54ca88dd7a88a242dde4ad38
Nathanael See <nathanaelsee@yahoo.com>
2018-11-07 00:16:25 -0800

    Pass new message count as props

--- src/components/TabBar.js
+++ src/components/TabBar.js
@@ -8,7 +8,7 @@ export default class TabBar extends Component {
     return (
       <Menu size="huge" fixed='bottom' fluid widths={3}>
         <Menu.Item as={Link} to="/eggs">
-          Eggs <Label color="red" content="0" circular />
+          Eggs<Label color="red" content={this.props.alerts ? this.props.alerts : 0} circular />
         </Menu.Item>
         <Menu.Item as={Link} to="/chickens" name="Chickens" />
         <Menu.Item as={Link} to="/lay" name="Lay" />
