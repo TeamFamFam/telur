be9a19f6f6897f8713f393c31aa970eb4710d821
Nathanael See <nathanaelsee@yahoo.com>
2018-11-08 16:42:33 -0800

    Added db accesses/modifications

--- src/components/Lay.js
+++ src/components/Lay.js
@@ -10,18 +10,50 @@ import {
   Step
 } from 'semantic-ui-react';
 
+import db from '../db.js';
+
 export default class Farm extends Component {
 
   state = {
-    writing: true,
-    message: ""
+    step: "Write",
+    text: "",
+    friends: ({})
   }
 
-  handleChange = (event) => this.setState({ message: event.target.value });
+  componentDidMount() {
+    let friendInfo = db.getFriends(this.props.user.user_id);
+    let friends = [];
+    for(var friend in friendInfo) {
+      var f = {
+        key: friendInfo[friend].username, 
+        value: friendInfo[friend].user_id, 
+        text: friendInfo[friend].username
+      };
+      friends = [...friends, f];
+    }
+    this.setState({friends});
+  }
 
-  swapStep = () => {
-    let writing = !this.state.writing;
-    this.setState({ writing });
+  handleChange = (event) => this.setState({ text: event.target.value });
+
+  swapWrite = () => {
+    this.setState({ step: "Write" });
+  }
+
+  swapSend = () => {
+    this.setState({ step: "Send" });
+  }
+
+  sendMessage = () => {
+    let sender_id = this.props.user.user_id;
+    let text = this.state.text;
+    let timestamp = Date.now();
+    let message = {
+      text,
+      sender_id,
+      timestamp
+    }
+    console.log(message);
   }
 
   render() {
@@ -31,27 +63,33 @@ export default class Farm extends Component {
           <Segment vertical basic>
             <Header>Lay an Egg</Header>
             <Step.Group unstackable attached="top" size="mini">
-              <Step title="Write" icon="edit" active={this.state.writing} onClick={this.swapStep} />
-              <Step title="Send" icon="send" active={!this.state.writing} onClick={this.swapStep} />
+              <Step title="Write" icon="edit" active={this.state.step === "Write"} onClick={this.swapWrite} />
+              <Step title="Send" icon="send" active={this.state.step === "Send"} onClick={this.swapSend} />
             </Step.Group>
-            {this.state.writing
+            {this.state.step === "Write"
               ? <div>
                 <Segment attached as={Form}>
                   <TextArea
                     autoHeight
                     placeholder="Write your message here..."
                     rows={3}
-                    value={this.state.message}
+                    value={this.state.text}
                     onChange={this.handleChange} />
                 </Segment>
                 <Button.Group attached="bottom">
                   <Button>Save Draft</Button>
-                  <Button primary icon="right arrow" labelPosition="right" content="Next" onClick={this.swapStep} />
+                  <Button secondary icon="right arrow" labelPosition="right" content="Next" onClick={this.swapSend} />
                 </Button.Group>
               </div>
               : <div>
-                Sending settings here
-                <Button secondary icon="left arrow" labelPosition="left" content="back" onClick={this.swapStep} />
+                <Segment attached as={Form}>
+                  <Form.Dropdown placeholder="Choose recipient(s)..." fluid multiple search selection options={this.state.friends} />
+                </Segment>
+                <Button.Group attached="bottom" widths={3}>
+                  <Button secondary icon="left arrow" labelPosition="left" content="Back" onClick={this.swapWrite} />
+                  <Button>Save Draft</Button>
+                  <Button primary icon="right arrow" labelPosition="right" content="Send" onClick={this.sendMessage} />
+                </Button.Group>
               </div>
             }
             <Image centered size="small" src="/egg.svg" verticalAlign="bottom" />
